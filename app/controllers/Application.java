1ea769b82625c79e5ed7a902b9a1dfdb1cbe5eda
Lucas Toledo Pereira <toledo_lu@hotmail.com>
2016-02-18 17:54:35 -0200

    chat listener

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import models.UserVO;
@@ -12,6 +14,7 @@ import com.google.gson.reflect.TypeToken;
 import play.*;
 import play.mvc.*;
 import views.html.*;
+import play.libs.EventSource;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http.RequestBody;
@@ -20,14 +23,81 @@ import play.mvc.Result;
 public class Application extends Controller {
 	
 	HashMap<String, UserVO> memberList = new HashMap<String, UserVO>();
+	
+	/** Keeps track of all connected browsers per room **/
+	  private static Map<String, List<EventSource>> socketsPerRoom = new HashMap<String, List<EventSource>>();
+	  
 
     public Result index() {
         return ok(index.render());
     }
+    
+    /**
+     * Controller action for POSTing chat messages
+     */
+    public Result postMessage() {
+      sendEvent(request().body().asJson());
+      return ok();
+    }
+    
+    /**
+     * Send event to all channels (browsers) which are connected to the room
+     */
+    public void sendEvent(JsonNode msg) {
+      String room  = msg.findPath("room").textValue();
+      if(socketsPerRoom.containsKey(room)) {
+        socketsPerRoom.get(room).stream().forEach(es -> es.send(EventSource.Event.event(msg)));
+      }
+    }
+    
+    
+    /**
+     * Establish the SSE HTTP 1.1 connection.
+     * The new EventSource socket is stored in the socketsPerRoom Map
+     * to keep track of which browser is in which room.
+     *
+     * onDisconnected removes the browser from the socketsPerRoom Map if the
+     * browser window has been exited.
+     * @return
+     */
+    public Result chatFeed(String room) {
+      String remoteAddress = request().remoteAddress();
+      Logger.info(remoteAddress + " - SSE conntected");
+
+      return ok(new EventSource() {
+        @Override
+        public void onConnected() {
+          EventSource currentSocket = this;
+
+          this.onDisconnected(() -> {
+            Logger.info(remoteAddress + " - SSE disconntected");
+            socketsPerRoom.compute(room, (key, value) -> {
+              if(value.contains(currentSocket))
+                value.remove(currentSocket);
+              return value;
+            });
+          });
+
+          // Add socket to room
+          socketsPerRoom.compute(room, (key, value) -> {
+            if(value == null){
+            	 ArrayList<EventSource> currentArray = new ArrayList<EventSource>();
+            	 currentArray.add(currentSocket);
+              return currentArray;
+            }else
+              value.add(currentSocket); return value;
+          });
+        }
+      });
+    }
+    
 	
+    
+    
 	public Result getMembers(){
 		return ok(Json.toJson(memberList));
 	}
+	
 	
 	
 	 public Result addMember() {
--- public/javascripts/chatController.js
+++ public/javascripts/chatController.js
@@ -6,7 +6,9 @@ app.controller("chatController", function($scope, $http, $location){
 	$scope.userName = $scope.$parent.userName;
 	$scope.userImageUrl = $scope.$parent.userImageUrl;
 //	$scope.userImageUrl = "http://www.imagensgratis.blog.br/imagens/imagens-imagens-que-se-mexem-para-celular-16.jpg";
-	$scope.userEmail = $scope.$parent.userEmail
+	$scope.userEmail = $scope.$parent.userEmail;
+	$scope.currentRoom = {name: 'Room 1', value: 'room1'};	
+	$scope.msgs = [];
 	
 	$http.get("/getMembers").success(function(data){
 		debugger;
@@ -18,4 +20,29 @@ app.controller("chatController", function($scope, $http, $location){
 			$location.path("/login");
 		}
 	}
+	
+	
+	
+    /** handle incoming messages: add to messages array */
+    $scope.addMsg = function (msg) {
+        $scope.$apply(function () { $scope.msgs.push(JSON.parse(msg.data)); });
+    };
+    
+    
+    /** posting chat text to server */
+    $scope.submitMsg = function () {
+        $http.post("/chat", { text: $scope.inputText, user: $scope.userName,
+            time: (new Date()).toUTCString(), room: $scope.currentRoom.value });
+        $scope.inputText = "";
+    };
+    
+	
+    /** start listening on messages from selected room */
+    $scope.listen = function () {
+        $scope.chatFeed = new EventSource("/chatFeed/" + $scope.currentRoom.value);
+        $scope.chatFeed.addEventListener("message", $scope.addMsg, false);
+    };
+    
+    $scope.listen();
+    
 });
